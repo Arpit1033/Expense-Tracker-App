a39781dc68000f8ef688742c41667d6a53009012
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-19 18:53:36 +0530

    Updated base.html and settings.py file

--- expanse_tracker_prj/expanse_tracker_prj/settings.py
+++ expanse_tracker_prj/expanse_tracker_prj/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -139,7 +140,7 @@ LOGOUT_URL = "logout"
 LOGIN_REDIRECT_URL = "index"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 AUTHENTICATION_BACKENDS = [
