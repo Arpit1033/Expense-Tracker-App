0843f38c3353ede8e4019ecfa96537496396aa7e
Arpit1033 <lcb2022015@iiitl.ac.in>
2025-04-16 15:13:24 +0530

    Add about page URL to urlpatterns

--- expanse_tracker_prj/myapp/urls.py
+++ expanse_tracker_prj/myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('edit/<int:id>/',views.edit,name='edit'),
     path('delete/<int:id>/',views.delete,name='delete'),
     path('home/',views.home,name="home"),
+    path('about/',views.about,name="about"),
 ]
